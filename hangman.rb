9ea58f8c27b0eb26d4f8faf74f5438351eba9dd3
April Blair <a.m.blair@eagle.clarion.edu>
2017-03-22 16:11:08 -0400

    helpers loop and new_game method

--- hangman.rb
+++ hangman.rb
@@ -18,8 +18,25 @@ post "/" do
 	erb :index
 end
 
-lives = 7
-while (lives >=1) do
+helpers do
+
+	def new_game
+
+		secret_word=""
+
+		while (secret_word.size<5 || secret_word.size>12) do
+		dictionary=File.readlines "5desk.txt"
+		dictionary.map! {|i| i.downcase.chomp!}
+		secret_word=dictionary[rand(dictionary.size)]
+		end
+		@secret_word=secret_word
+		session[:secret_word]=@secret_word.upcase
+		session[:guessed]=[]
+		session[:incorrect_guesses]=0
+		session[:win]=false
+		session[:lose]=false
+		session[:partial_word]="_"*@secret_word.size
+	end
 
 
 end
